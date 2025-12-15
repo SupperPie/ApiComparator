2f7cde67d2cab9da38bef372fe01c7e1978de5eb
du yufei <duyufei@dus-MacBook-Air.local>
2025-12-15 17:01:17 +0800

    streamlit adjust

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ PORT=8501
 # Run Streamlit
 # --server.headless true disables the interactive browser opening on server
 echo "Starting Streamlit App on port $PORT..."
-streamlit run app.py --server.port $PORT --server.headless true
+python3 -m streamlit run app.py --server.port $PORT --server.headless true
